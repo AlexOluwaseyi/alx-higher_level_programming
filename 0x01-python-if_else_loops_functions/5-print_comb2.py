3d81f3dbf991b712d7bc0e57b5a0f5a23065a996
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-08-10 12:49:57 -0700

    Update: Added 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-for i in range(98):
-    while i != 98:
-          print("{0:02d}".format(i), end=", ")
-          i += 1
+for i in range(99):
+    while i != 99:
+        print("{0:02d}".format(i), end=", ")
+        i += 1
     break
 print("{0:02d}".format(i))
