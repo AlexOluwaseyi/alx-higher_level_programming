d711cbe642055e95dc81336ebc9f8b92675c2afe
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-08-10 13:23:04 -0700

    Combination of non-similar numbers

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for i in range(10):
+    for j in range(10):
+        if (i == 8 and j ==9):
+            print("{}{}".format(i, j))
+        elif i != j and i < j:
+            print("{}{}".format(i, j), end=", ")
