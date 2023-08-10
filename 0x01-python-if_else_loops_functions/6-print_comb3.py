7ece86a57c78e6273771f1ec218cdd96aa7a4090
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-08-10 13:24:35 -0700

    Updated: whitespace error

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 
 for i in range(10):
     for j in range(10):
-        if (i == 8 and j ==9):
+        if (i == 8 and j == 9):
             print("{}{}".format(i, j))
         elif i != j and i < j:
             print("{}{}".format(i, j), end=", ")
