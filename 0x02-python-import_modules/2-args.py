7fb5315176236c69b96fbc9c5a9d11e150b745c3
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-08-11 00:43:16 -0700

    Updated: Plural forms for zero, one or more variables

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,15 @@ from sys import argv
 if __name__ == "__main__":
     n = len(argv)
 
-    if n > 1:
+    if n > 2:
         print("{:d} arguments:".format(n - 1))
         for i in range(n):
             if i > 0:
                 print("{:d}: {}".format(i, argv[i]))
-    elif n == 1:
+    elif n == 2:
         print("{:d} argument.".format(n - 1))
+        for i in range(n):
+            if i > 0:
+                print("{:d}: {}".format(i, argv[i]))
+    elif n == 1:
+        print("{:d} arguments.".format(n - 1))
