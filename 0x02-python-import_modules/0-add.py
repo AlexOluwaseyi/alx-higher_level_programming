ad6956367e92a0dd89c9ab5ddf562a6b2386dc4f
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-08-10 23:40:24 -0700

    Added a main block

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
 from add_0 import add
+if __name__ == "__main__":
+    a = 1
+    b = 2
 
-a = 1
-b = 2
+    add(a, b)
 
-add(a, b)
-
-print("{} + {} = {}".format(a, b, add(a, b)))
-#str(a) + " + " + str(b) + " = " + str(add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
