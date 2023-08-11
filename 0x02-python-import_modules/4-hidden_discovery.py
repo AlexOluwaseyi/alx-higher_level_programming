4aefe4df91e06a0711cde3c89cbddf64a03cecfe
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-08-11 01:34:59 -0700

    Names in a module

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import hidden_4
+if __name__ == "__main__":
+    names = dir(hidden_4)
+
+    for i in names:
+        if i[:2] != "__":
+            print(i)
+#    if names[:2] != "__":
+#         print(names)
