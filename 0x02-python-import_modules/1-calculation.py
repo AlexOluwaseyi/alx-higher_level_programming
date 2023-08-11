9c5c57df3c5553659bcd6282aff67a14b3593323
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-08-11 00:02:03 -0700

    Updated: Format specifier for integers

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     a = 10
     b = 5
 
-    print("{} + {} = {}".format(a, b, cal.add(a, b)))
-    print("{} - {} = {}".format(a, b, cal.sub(a, b)))
-    print("{} * {} = {}".format(a, b, cal.mul(a, b)))
-    print("{} / {} = {}".format(a, b, cal.div(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, cal.add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, cal.sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, cal.mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, cal.div(a, b)))
