da762e32f783406d087a18e002db7f5e69479be2
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-08-11 00:38:10 -0700

    Argv in Python

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from sys import argv
+if __name__ == "__main__":
+    n = len(argv)
+
+    if n > 1:
+        print("{:d} arguments:".format(n - 1))
+        for i in range(n):
+            if i > 0:
+                print("{:d}: {}".format(i, argv[i]))
+    elif n == 1:
+        print("{:d} argument.".format(n - 1))
