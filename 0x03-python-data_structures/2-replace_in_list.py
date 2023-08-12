5240fd6aced6cc96a500142eb619446fa65b4d6b
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-08-12 02:30:01 -0700

    Replace lement at index

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    n = len(my_list) - 1
+    if 0 <=  idx <=  n:
+        my_list[idx] = element
+        return my_list
+    else:
+        return my_list
