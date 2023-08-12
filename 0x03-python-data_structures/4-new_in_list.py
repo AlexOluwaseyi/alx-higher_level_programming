a70ef2c85b8744a91f56014a74ebe68bf1843f09
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-08-12 02:54:13 -0700

    New in list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    n = len(my_list)
+    if 0 <= idx < len(my_list):
+        new_list = my_list.copy()
+        new_list[idx] = element
+        return new_list
+    else:
+        return my_list
