29b1ffce0b261b61a87cc6c09fc40e09b1700ee4
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-08-12 05:36:13 -0700

    Tuples

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    length = len(sentence)
+    if length == 0:
+        first = None
+    else:
+        first = sentence[0]
+    my_tuple = (length, first)
+    return my_tuple
+
