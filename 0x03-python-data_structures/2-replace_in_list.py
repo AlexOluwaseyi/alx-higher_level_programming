1a62f47ad0fc7321c37ae0d48181421386c518ed
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-08-12 02:31:48 -0700

    Corrected: Pycodestyle error

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 
 def replace_in_list(my_list, idx, element):
     n = len(my_list) - 1
-    if 0 <=  idx <=  n:
+    if 0 <= idx <= n:
         my_list[idx] = element
         return my_list
     else:
