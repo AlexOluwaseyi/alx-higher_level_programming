501ac821db3ba309176fa372c78464b43724bcae
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-08-28 04:55:20 -0700

    Reviewed: Error to STDERR

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,6 +5,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except Exception as e:
-        print("Exception: ", e, file=sys.stderr)
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return False
     return True
