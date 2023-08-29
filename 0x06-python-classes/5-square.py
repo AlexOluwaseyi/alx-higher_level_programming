f00c7a540596b1697780f765def3268ad614b8c5
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-08-29 03:04:27 -0700

    What is Matrix without Keanu Reeves?

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,4 +1,4 @@
-#!/usr.bin/python3
+#!/usr/bin/python3
 
 class Square:
     def __init__(self, size=0):
@@ -18,3 +18,10 @@ class Square:
 
     def area(self):
         return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
