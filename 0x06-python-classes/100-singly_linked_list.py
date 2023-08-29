bd866b6b0fd450c1eac910ebc0e6bc06aa8c5610
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-08-29 04:38:24 -0700

    Comment section Reviewed

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Define classes for a singly-linked list."""
 
+
 class Node:
     """Represent a node in a singly-linked list."""
     def __init__(self, data, next_node=None):
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Define a class Square."""
 
+
 class Square:
     """Represent a square."""
     def __init__(self, size=0):
