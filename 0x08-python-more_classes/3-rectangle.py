e78e0b1b77e4de0eebbeb802419733ada40fbb47
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-09-04 23:12:46 -0700

    Pycodestyle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -115,8 +115,7 @@ class Rectangle:
 
         """
         if self.__width == 0 or self.__height == 0:
-            return ""
-        
+            return "" 
         rectangle_str = ""
         for _ in range(self.__height):
             rectangle_str += "#" * self.__width + "\n"
