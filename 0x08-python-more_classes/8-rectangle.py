9be469c4071fbc35c518ceb0b062316286d0c98a
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-09-05 00:26:40 -0700

    Comparison

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""7-rectangle, built for Holberton Python project 0x08 task 1.
+"""8-rectangle, built for Holberton Python project 0x08 task 1.
 """
 
 
@@ -143,3 +143,18 @@ class Rectangle:
         """
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+
+        area_1 = rect_1.area()
+        area_2 = rect_2.area()
+
+        if area_1 >= area_2:
+            return rect_1
+        else:
+            return rect_2
