fbb1b253337c2ab6637c13963dd5fd891933843a
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-09-11 18:15:27 -0700

    Geometry module

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""An empty class"""
+
+
+class BaseGeometry:
+    """
+    This is a base class for geometry-related operations.
+
+    It provides a foundation for defining geometric shapes and performing
+    calculations related to them.
+    """
+    pass
