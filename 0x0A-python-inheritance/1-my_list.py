fd2ebcaddd0529fbc57d7cd4150783970125bf95
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-09-11 17:14:46 -0700

    Fixed: Use of 'from' keyword'
    Removed 'from' to prevent being detected as an import
    
    Done

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
 
 """
-A script to illustrate a class inheriting from list
+A script to illustrate a class inheriting 'list'
 """
 
 class MyList(list):
     """
-    A custom list class that inherits from the built-in list class.
-
-    Pycodestyle (PEP8) Compliance:
-    - Class names are written in CamelCase.
-    - Methods are written in lowercase with words separated by underscores.
-    - Opening and closing parentheses in method definitions are not spaced.
-    - Method docstrings are enclosed in triple quotes.
+    A custom list class that inherits the built-in list class.
 
     Public Methods:
         - print_sorted(self): Prints the list in ascending sorted order.
