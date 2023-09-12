41bce9fc90c27f992235ec5f450714edaff73b0a
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-09-11 18:27:18 -0700

    Still geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+class BaseGeometry:
+    """
+    A base class for geometry-related operations.
+
+    This class provides a foundation for defining geometric shapes and performing
+    calculations related to them.
+
+    Methods:
+        area(): Raises an Exception with the message 'area() is not implemented'.
+            This method is meant to be overridden by derived classes to
+            provide specific implementation for calculating area.
+    """
+    def area(self):
+        """
+        Raises an Exception with the message 'area() is not implemented'.
+
+        This method is meant to be overridden by derived classes to
+        provide specific implementation for calculating area.
+        """
+        raise Exception('area() is not implemented')
