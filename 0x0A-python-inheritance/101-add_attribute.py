5c12b6860505a0fb2733acd58c737f8ed5e993ce
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-09-11 19:17:56 -0700

    Update

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""Defines a class MyInt that inherits from int."""
+"""Defines a function that adds attributes to objects."""
 
 
-class MyInt(int):
-    """Invert int operators == and !=."""
+def add_attribute(obj, att, value):
+    """Add a new attribute to an object if possible.
 
-    def __eq__(self, value):
-        """Override == opeartor with != behavior."""
-        return self.real != value
-
-    def __ne__(self, value):
-        """Override != operator with == behavior."""
-        return self.real == value
+    Args:
+        obj (any): The object to add an attribute to.
+        att (str): The name of the attribute to add to obj.
+        value (any): The value of att.
+    Raises:
+        TypeError: If the attribute cannot be added.
+    """
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, att, value)
