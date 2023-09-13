ab773f4a6034915d58182b650d94ab99684ad471
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-09-12 23:36:21 -0700

    Class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+function that returns the dictionary description
+with simple data structure
+"""
+
+def class_to_json(obj):
+    """
+    Convert an object with serializable attributes to a dictionary.
+
+    Args:
+        obj: An instance of a Class.
+
+    Returns:
+        dict: A dictionary description of the object.
+    """
+    if not hasattr(obj, '__dict__'):
+        raise ValueError("Input object must be an instance of a Class.")
+
+    return obj.__dict__
+
