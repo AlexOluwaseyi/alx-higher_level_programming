0fe7cd2f5ff0a164c4fc5b0cf47f998daebdc00e
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-09-12 23:52:05 -0700

    Classes and JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+A class of students
+converted to json
+"""
+
+
+class Student:
+    """Class Student defined below"""
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a Student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieve a dictionary representation of a Student instance.
+
+        Returns:
+            dict: A dictionary representing the Student instance.
+        """
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+            }
