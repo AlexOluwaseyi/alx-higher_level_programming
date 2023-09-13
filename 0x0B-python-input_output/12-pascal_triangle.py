cc921fa9b2c0a6f4eb928176b10c3e96854331f6
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-09-13 00:23:28 -0700

    Pascal II

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -5,6 +5,10 @@ A pascal triangle function
 
 
 def pascal_triangle(n):
+    """
+    Returns a list of lists of integers
+    representing the Pascal’s triangle of n.
+    """
     if n <= 0:
         return []
 
