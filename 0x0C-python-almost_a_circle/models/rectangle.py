1cb7a0933a57e499a947470e7fabb49ec390323d
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-09-19 01:41:40 -0700

    ..

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -38,7 +38,7 @@ class Rectangle(Base):
     def width(self, value):
         """Assigns 'value' to width"""
         if type(value) is not int:
-            raise TypeError("width must be integer")
+            raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
@@ -52,7 +52,7 @@ class Rectangle(Base):
     def height(self, value):
         """Assigns 'value' to height"""
         if type(value) is not int:
-            raise TypeError("height must be integer")
+            raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
@@ -66,7 +66,7 @@ class Rectangle(Base):
     def x(self, value):
         """Assigns 'value' to x"""
         if type(value) is not int:
-            raise TypeError("x must be integer")
+            raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
@@ -80,7 +80,7 @@ class Rectangle(Base):
     def y(self, value):
         """Assigns 'value' to y"""
         if type(value) is not int:
-            raise TypeError("y must be integer")
+            raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
