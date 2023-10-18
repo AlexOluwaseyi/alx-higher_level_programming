1e013dbee05ae179f61fac43f57926e2ac1f6d91
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-10-18 08:46:50 -0700

    Hotfix: **

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,10 +1,9 @@
 -- Creates a db and table with p_k an f_k
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
--- USE hbtn_0d_usa,
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-	id INT NOT NULL UNIQUE,
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+	id INT NOT NULL UNIQUE PRIMARY KEY,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.cities(id)
+	FOREIGN KEY (state_id) REFERENCES cities(id)
 	);
