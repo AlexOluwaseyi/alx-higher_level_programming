372c8b3f0a46635e999feadd10ed4fb07e9837d8
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-10-17 21:29:05 -0700

    Updates: 11-best_score.sql 12-no_cheating.sql

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,2 @@
+-- Lists all rows of a table from the database
+SELECT score, name FROM second_table WHERE score>= 10 ORDER BY score DESC
---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,2 @@
+-- Update score based on na,e
+UPDATE second_table SET score=10 where name='Bob';
