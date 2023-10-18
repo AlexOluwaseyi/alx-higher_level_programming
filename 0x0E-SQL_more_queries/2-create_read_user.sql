687979dbd021f6a463b9a1e2f56558c93f402ebf
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-10-18 08:38:28 -0700

    Updated: 2-create_read_user.sql 7-cities.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
 -- Creates database hbtn_0d_2 and user_0d_2
 -- Assigns SELECT privilege in the database hbtn_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS user_0d_2@localhost;
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
 ON hbtn_0d_2.*
 TO user_0d_2@localhost;
