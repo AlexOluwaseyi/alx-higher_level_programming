8e0b2aa64e72d0c6e33def004498b60be01afa96
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-10-17 20:48:53 -0700

    Updates: 5-full_table.sql 6-list_values.sql 7-insert_value.sql 8-count_89.sql

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- Full description of the table from a database
+-- in an sql server
+SHOW CREATE TABLE first_table;
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+-- Lists all rows of a table from the database
+SELECT * FROM first_table
