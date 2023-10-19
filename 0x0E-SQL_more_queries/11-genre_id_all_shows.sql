45dcf411a2011ecee1aacd2453047c628ea75d93
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-10-18 21:07:48 -0700

    Completed: All Mandatory

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- sql
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres RIGHT OUTER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC
