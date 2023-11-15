6562f317e91d21a60c7c4f9d0870cecf7e979cc5
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-11-14 19:59:58 -0800

    Updated: Semistandard Compliance

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -8,19 +8,19 @@ module.exports = class Rectangle {
     }
   }
 
-  print() {
+  print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
 
-  rotate() {
+  rotate () {
     const temp = this.width;
     this.width = this.height;
     this.height = temp;
   }
 
-  double() {
+  double () {
     this.width *= 2;
     this.height *= 2;
   }
