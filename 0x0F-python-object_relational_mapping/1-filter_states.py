35506852bdd3b50e2b174ec9fa777b4efea392a1
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-11-17 16:21:23 -0800

    States starting with Cap 'N'

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+'''
+a script that lists all states with name starting with N (upper N)
+from the database hbtn_0e_0_usa
+takes 3 arguments: mysql username, mysql password and database name
+'''
+
+if __name__ == "__main__":
+    import MySQLdb
+    from os import sys
+
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+            host="localhost", port=3306,
+            user=sys.argv[1], passwd=sys.argv[2],
+            db=sys.argv[3]
+            )
+
+    # Get a cursor object
+    cursor = db.cursor()
+
+    # Execute the query to list all states
+    cursor.execute(
+            "SELECT * FROM states WHERE name\
+            LIKE 'N%' ORDER BY id ASC"
+            )
+
+    # Fetch all the results
+    results = cursor.fetchall()
+
+    # Display the results
+    for row in results:
+        print(row)
+
+    # Close the cursor and connection
+    cursor.close()
+    db.close()
