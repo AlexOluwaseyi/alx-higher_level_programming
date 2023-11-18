c316f3a0b3ed7d20994ecb8b1e233df77977a37f
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-11-17 16:50:05 -0800

    SQL Injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+'''
+Same script, but better - Prevent/avoid SQL Injection
+
+a script that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument.
+
+takes 4 arguments: mysql username, mysql password,
+database name and state name
+'''
+
+if __name__ == "__main__":
+    import MySQLdb
+    from os import sys
+
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+            host="localhost", port=3306,
+            user=sys.argv[1], passwd=sys.argv[2],
+            db=sys.argv[3]
+            )
+
+    # The name of the state to be queried
+    stateName = sys.argv[4]
+
+    # Get a cursor object
+    cursor = db.cursor()
+
+    # Execute the query to list all states with same
+    cursor.execute(
+            "SELECT * FROM states WHERE name\
+            LIKE '%{stateName}%' ORDER BY states.id ASC;"
+            )
+
+    # Fetch all the results
+    results = cursor.fetchall()
+
+    # Display the results
+    for row in results:
+        print(row)
+
+    # Close the cursor and connection
+    cursor.close()
+    db.close()
