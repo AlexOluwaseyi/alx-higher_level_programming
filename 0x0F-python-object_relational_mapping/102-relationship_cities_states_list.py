b6c88bed9d5a6ee964cd0465c15f8de0530dec3d
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-11-19 15:23:21 -0800

    Updated imports

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -3,14 +3,15 @@
 from the database hbtn_0e_101_usa
 """
 
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from relationship_state import State
-from relationship_city import City
-
 
 if __name__ == "__main__":
+    import sys
+    from relationship_state import Base, State
+    from relationship_city import City
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from sqlalchemy.schema import Table
+
     # Create a SQLAlchemy engine
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
