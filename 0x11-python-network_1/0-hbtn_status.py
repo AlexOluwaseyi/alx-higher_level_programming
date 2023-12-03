94546734cf0fd669ec3a36b9551d4b4fa5b0a31b
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-12-03 00:02:41 -0800

    Update  task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,13 +7,15 @@ a Python script that fetches https://alx-intranet.hbtn.io/status
 if __name__ == "__main__":
     import urllib.request
 
-    url = 'https://alx-intranet.hbtn.io/status'
+    url = 'http://0.0.0.0:5000/status'
+    try:
+        with urllib.request.urlopen(url) as response:
+            content = response.read()
+            utf8_content = content.decode('utf-8')
 
-    with urllib.request.urlopen(url) as response:
-        content = response.read()
-        utf8_content = content.decode('utf-8')
-
-        print("Body response:")
-        print("    - type:", type(content))
-        print("    - content:", content)
-        print("    - utf8 content:", utf8_content)
+            print("Body response:")
+            print("\t- type:", type(content))
+            print("\t- content:", content)
+            print("\t- utf8 content:", utf8_content)
+    except urllib.error.URLError as e:
+        print("Error: ", e.reason)
