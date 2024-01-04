6e89fb586fd94afe0d47ff8f31db687354416176
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-01-04 09:47:53 -0800

    Semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,10 +4,10 @@ const file = process.argv[2];
 const fs = require('fs');
 
 fs.readFile(file, 'utf-8', (err, data) => {
-	if (err) {
-		console.error(err);
-		process.exit(1);
-	}
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
 
-	console.log(data);
+  console.log(data);
 });
