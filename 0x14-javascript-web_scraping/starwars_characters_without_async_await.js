1220db7a1f98320ff9e30f1dc3118b26e26b3803
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-01-05 00:27:01 -0800

    Await/Async

--- 0x14-javascript-web_scraping/starwars_characters_without_async_await.js
+++ 0x14-javascript-web_scraping/starwars_characters_without_async_await.js
@@ -10,11 +10,11 @@ const getCharacterData = async (characterUrl) => {
   return new Promise((resolve, reject) => {
     request(characterUrl, (error, response, body) => {
       if (error) {
-        reject(error);
-      } else {
-        resolve(JSON.parse(body).name);
-      }
-    });
+        console.error(error);
+		  process.exit(1);
+	  }
+      resolve(JSON.parse(body).name);
+	});
   });
 };
 
@@ -27,17 +27,23 @@ request(baseUrl, async (error, response, body) => {
   const movie = JSON.parse(body);
 
   for (const character of movie.characters) {
-    try {
-      const characterName = await getCharacterData(character);
-      sortedCharacters.push(characterName);
-    } catch (error) {
-      console.error(error);
-      process.exit(1);
-    }
+    request(character, (error, response, body) => {
+      if (error) {
+        console.error(error);
+        process.exit(1);
+      }
+//      const characterObject = JSON.parse(body);
+//      const characterName = characterObject.name;
+//      sortedCharacters.push(characterName);
+//      console.log(sortedCharacters);
+//      console.log(characterObject.name);
+	  const characterName = await getCharacterData(character);
+	  sortedCharacters.push(characterName);
+    });
+	  console.log(sortedCharacters);
   }
-
+  console.log(sortedCharacters);
   sortedCharacters.forEach(item => {
     console.log(item);
   });
 });
-
